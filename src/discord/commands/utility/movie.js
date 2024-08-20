ff61e163242e2769166f8ee4e4f24f5931e3abed
Tim Bedsole <23228051+Timothyb92@users.noreply.github.com>
2024-08-20 00:02:52 -0400

    added /movie command to discord bot, successfully hitting TMDB API with a get request for a movie title

---
+++ src/discord/commands/utility/movie.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require('discord.js');
+const axios = require('axios');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('movie')
+    .setDescription('Gets a movie from TMDB api')
+    .addStringOption(option => 
+        option.setName('title')
+      .setDescription('The movie title to search')
+      .setRequired(true)),
+  async execute(interaction) {
+    const movieTitle = interaction.options.getString('title', true).toLowerCase();
+    //TODO look into interaction.options - this is nebulous to me
+
+    await interaction.deferReply();
+    const titleSearchResults = await axios.get(`http://localhost:3000/movies/${movieTitle}`)
+    console.log(titleSearchResults.data.results);
+    await interaction.editReply(`Logging search results`);
+  },
+};
